aef8879d32ae1cbd250c7048bde72bc5d5303a6a
Jorge Salinas <94187187+jorgesalinas07@users.noreply.github.com>
2024-02-02 10:21:31 -0500

    REQ-4-implement-price-filter (#4)
    feat: Implement price filter

--- src/components/ResultInfoBar/SortFilter/index.jsx
+++ src/components/ResultInfoBar/SortFilter/index.jsx
@@ -1,10 +1,22 @@
+import { useContext } from 'react';
+import { SearchContext } from '../../../contexts/SearchContext'
 import './SortFilter.css'
 
 function SortFilter () {
+    const {
+        setPriceSortOrder,
+    } = useContext(SearchContext);
+
     return (
         <div className='SortFilterContainer'>
-            <select name="order" id="order">
-                <option value="Name">Name</option>
+            <select
+                name="order"
+                id="order"
+                onChange={(event) => {
+                    setPriceSortOrder(event.target.value);
+                }}
+                >
+                <option value="Default">Name</option>
                 <option value="Price_Low">Price: Low to High</option>
                 <option value="Price_High">Price: High to Low</option>
             </select>
