a038442bcec33542832726bbe1430edab38f1336
Jorge Salinas <ext.jsalinas@warbyparker.com>
2024-02-02 11:22:10 -0500

    feat: Implement rate filter

--- src/components/Filter/RatingFilter/index.jsx
+++ src/components/Filter/RatingFilter/index.jsx
@@ -2,14 +2,19 @@ import { Rating } from './Rating'
 import './RatingFilter.css'
 
 function RatingFilter () {
+    const ratings = [4, 3, 2, 1];
     return (
         <div className='RatingFilterContainer'>
             <h2>Rates:</h2>
-            <div className='RatingsContainer'>
-                <Rating stars={4} />
-                <Rating stars={3} />
-                <Rating stars={2} />
-                <Rating stars={1} />
+            <div className='RatingsContainer'> {
+                ratings.map((stars) => (
+                    <Rating
+                        key={stars}
+                        stars={stars}
+                        isRateFilterClick={true}
+                    />
+                ))
+            }
             </div>
         </div>
     )
